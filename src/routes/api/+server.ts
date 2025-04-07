b8a71a255c1ed19fc94f64430f173925bf99469f
thuy tran van <thuytranvan.gl@gmail.com>
2025-04-07 23:15:29 +0700

    add more error handling

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,11 +1,20 @@
 import path from 'path';
-import { error, type RequestHandler } from '@sveltejs/kit';
+import { error, HttpError_1, type RequestHandler } from '@sveltejs/kit';
 import { writeFile, unlink } from 'fs/promises';
 import { RateLimiter } from 'sveltekit-rate-limiter/server';
 import { v4 as uuid  } from 'uuid';
 
 const staticDir = process.env.DEV === "true" ? "./static" : "/tmp";
 console.log("[DEBUG] ", { staticDir });
+
+async function resolve<T>(promise: Promise<T>): Promise<[null, T] | [Error, null]> {
+    try {
+        const res = await promise
+        return [null, res];
+    } catch (err: any) {
+        return [err, null];
+    }
+}
 
 const limiter = new RateLimiter({
   rates: {
@@ -27,21 +36,26 @@ function getFilePath(fileName: string) {
 
 export const POST: RequestHandler = async (event) => {
   if (await limiter.isLimited(event)) throw error(429);
+  event.setHeaders({ "Content-Type": "application/json" });
 
-  const formData = await event.request.formData();
+  const [err, formData] = await resolve<FormData>(event.request.formData());
+  if (err) {
+    throw new HttpError_1(400, "missing form data");
+  }
   const file = formData.get('image') as File | undefined;
-  const fileName = uuid().split("-")[0];
   if (!file) {
-    return new Response(JSON.stringify({ message: 'No file uploaded' }), {
-      status: 400,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    throw new HttpError_1(400, "No file uploaded");
+  }
+  const fileName = uuid().split("-")[0] + ".jpeg";
+  const [err1, buffer] = await resolve(file.arrayBuffer());
+  if (err1) {
+    throw new HttpError_1(500, "cannot get file buffer");
+  }
+  const [err2] = await resolve(writeFile(getFilePath(fileName), new Uint8Array(buffer)));
+  if (err2) {
+    throw new HttpError_1(500, "cannot writw file");
   }
 
-  const buffer = await file.arrayBuffer();
-  await writeFile(getFilePath(fileName), new Uint8Array(buffer));
   scheduleDelete(fileName, 60 * 3);
   return new Response(JSON.stringify({ fileName }), {
     headers: {
