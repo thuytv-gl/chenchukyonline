6e598d663f1d4596bc864b84ffb260ddd29851de
thuy tran van <thuytranvan.gl@gmail.com>
2025-04-08 15:28:05 +0700

    handling errors 7

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,38 @@
+<head>
 <title>Chèn Chữ Ký online</title>
+</head>
 
 <script lang="ts">
   import { browser } from "$app/environment";
   import { calcRatio, createImage, toDataUrl } from "$lib";
   import Canvas from "./Canvas";
   import { onMount } from 'svelte';
+
   import { createScreenLogger } from '$lib/debugConsole';
-  import { v4 as uuid  } from 'uuid';
-  
-  let screenLogger: ReturnType<typeof createScreenLogger>;
-  
-  onMount(() => {
-    if (browser) {
-      // Only initialize in browser environment
-      screenLogger = createScreenLogger({
-        position: 'bottom',
-        theme: 'dark',
-        maxLogs: 100
-      });
-      
-      return () => {
-        screenLogger.restore();
-      };
-    }
-  });
+
+  let debug: boolean = false;
+  if (browser) {
+    debug = new URLSearchParams(window?.location.href).get("debug") !== null;
+  }
+
+  if (debug) {
+    let screenLogger: ReturnType<typeof createScreenLogger>;
+    
+    onMount(() => {
+      if (browser) {
+        // Only initialize in browser environment
+        screenLogger = createScreenLogger({
+          position: 'bottom',
+          theme: 'dark',
+          maxLogs: 100
+        });
+        
+        return () => {
+          screenLogger.restore();
+        };
+      }
+    });
+  }
 
   type Step = "image" | "signature" | "edit" | "preview";
   let step: Step = "image";
@@ -39,15 +48,6 @@
     window.alert(msg);
   }
 
-  async function resolve<T>(promise: Promise<T>): Promise<[null, T] | [Error, null]> {
-      try {
-          const res = await promise
-          return [null, res];
-      } catch (err: any) {
-          return [err, null];
-      }
-  }
-
   async function createCanvas(step: Step, canvasRef: HTMLCanvasElement, image: File, signature: File) {
     if (!browser || step !== "edit" || !canvasRef) return;
     loading = true;
@@ -60,8 +60,8 @@
       return panic("Failed to import image and signature");
     }
 
-    const maxW = document.documentElement.clientWidth * 0.95;
-    const maxH = document.documentElement.clientHeight * 0.9;
+    const maxW = document.documentElement.clientWidth * 0.8;
+    const maxH = document.documentElement.clientHeight * 0.8;
     const ratio = calcRatio(fImage.width, fImage.height, maxW, maxH);
     canvasRef.width = fImage.width * ratio;
     canvasRef.height = fImage.height * ratio;
@@ -110,29 +110,7 @@
     step = "edit";
   }
 
-  function uploadFile(url: string, formData: FormData): Promise<string> {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-
-      xhr.open('POST', url);
-
-      xhr.onload = () => {
-        if (xhr.status >= 200 && xhr.status < 300) {
-          resolve(xhr.response);
-        } else {
-          reject(new Error(`Request failed with status ${xhr.status}: ${xhr.statusText}`));
-        }
-      };
-
-      xhr.onerror = () => {
-        reject(new Error('There was a network error.'));
-      };
-
-      xhr.send(formData); // No need to set Content-Type for FormData
-    });
-  }
-
-  function base64ToFile(dataUrl: string, filename: string) {
+  function base64ToFile(dataUrl: string) {
     // Extract the MIME type and base64 data
     const arr = dataUrl.split(',');
     const mimeMatch = arr[0].match(/:(.*?);/);
@@ -147,50 +125,14 @@
     }
 
     // Create a new File object
-    return new File([u8arr], filename, { type: mime });
-  }
-
-  function jsonParse(data: string): Promise<any> {     
-    return new Promise((rs, rj) => {
-        try {
-            rs(JSON.parse(data));
-        } catch(e: any) {
-            rj(e);
-          }
-      });
+    return new File([u8arr], Date.now().toString(), { type: mime });
   }
 
   async function preview() {
-    loading = true;
-    try {
-      const imageData = canvas.toDataURL({ format: "jpeg", multiplier, quality: 0.92 });
-      const imgFile = base64ToFile(imageData, uuid().split("-")[0]);
-      const body = new FormData();
-      body.append("image", imgFile);
-      const [err1, res] = await resolve<string>(uploadFile("/api", body));
-
-      if (err1) {
-        console.error("upload file");
-        throw err1;
-      }
-
-      const [err2, data] = await resolve<{ fileName: string }>(jsonParse(res));
-
-      if (err2) {
-        console.error("parse json");
-        throw err2;
-      }
-
-      if (!data.fileName) {
-        throw new Error("Missing fileName in response body");
-      }
-      loading = false;
-      return window.location.href = `/api/${data.fileName}`;
-    } catch(e: any) {
-      loading = false;
-      console.log(e);
-      return panic("Đã có lỗi xảy ra vui lòng thử lại sau! " + e.message);
-    }
+    const imageData = canvas.toDataURL({ format: "jpeg", multiplier, quality: 0.92 });
+    const imgFile = base64ToFile(imageData);
+    downloadUrl = window.URL.createObjectURL(imgFile);
+    step = "preview";
   }
 
   $: createCanvas(step, canvasRef, image, signature)
@@ -216,45 +158,46 @@
 {/if}
 
 {#if step === "edit"}
-  <div style={`display: ${loading ? "none" : "block"}`}>
     <div class="fcenter">
       <canvas bind:this={canvasRef} id="canvas"></canvas>
     </div>
 
-    <div class="fcenter">
-    <button class="preview-btn history-btn" on:click={() => canvas.undo()}>
-       <svg fill="#ffffff" height="18px" width="18px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 26.676 26.676" xml:space="preserve">
-          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-          <g id="SVGRepo_iconCarrier">
-             <g>
-                <path d="M26.105,21.891c-0.229,0-0.439-0.131-0.529-0.346l0,0c-0.066-0.156-1.716-3.857-7.885-4.59 c-1.285-0.156-2.824-0.236-4.693-0.25v4.613c0,0.213-0.115,0.406-0.304,0.508c-0.188,0.098-0.413,0.084-0.588-0.033L0.254,13.815 C0.094,13.708,0,13.528,0,13.339c0-0.191,0.094-0.365,0.254-0.477l11.857-7.979c0.175-0.121,0.398-0.129,0.588-0.029 c0.19,0.102,0.303,0.295,0.303,0.502v4.293c2.578,0.336,13.674,2.33,13.674,11.674c0,0.271-0.191,0.508-0.459,0.562 C26.18,21.891,26.141,21.891,26.105,21.891z"></path>
-             </g>
-          </g>
-       </svg>
-    </button>
-    <button class="preview-btn history-btn" on:click={() => canvas.redo()} style="transform:matrix(-1, 0, 0, 1, 0, 0);">
-       <svg fill="#ffffff" height="18px" width="18px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 26.676 26.676" xml:space="preserve" stroke="#ffffff">
-          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-          <g id="SVGRepo_iconCarrier">
-             <g>
-                <path d="M26.105,21.891c-0.229,0-0.439-0.131-0.529-0.346l0,0c-0.066-0.156-1.716-3.857-7.885-4.59 c-1.285-0.156-2.824-0.236-4.693-0.25v4.613c0,0.213-0.115,0.406-0.304,0.508c-0.188,0.098-0.413,0.084-0.588-0.033L0.254,13.815 C0.094,13.708,0,13.528,0,13.339c0-0.191,0.094-0.365,0.254-0.477l11.857-7.979c0.175-0.121,0.398-0.129,0.588-0.029 c0.19,0.102,0.303,0.295,0.303,0.502v4.293c2.578,0.336,13.674,2.33,13.674,11.674c0,0.271-0.191,0.508-0.459,0.562 C26.18,21.891,26.141,21.891,26.105,21.891z"></path>
-             </g>
-          </g>
-       </svg>
-    </button>
+    <div class="fcenter footer">
+      <button class="preview-btn history-btn" on:click={() => canvas.undo()}>
+         <svg fill="#ffffff" height="18px" width="18px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 26.676 26.676" xml:space="preserve">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+               <g>
+                  <path d="M26.105,21.891c-0.229,0-0.439-0.131-0.529-0.346l0,0c-0.066-0.156-1.716-3.857-7.885-4.59 c-1.285-0.156-2.824-0.236-4.693-0.25v4.613c0,0.213-0.115,0.406-0.304,0.508c-0.188,0.098-0.413,0.084-0.588-0.033L0.254,13.815 C0.094,13.708,0,13.528,0,13.339c0-0.191,0.094-0.365,0.254-0.477l11.857-7.979c0.175-0.121,0.398-0.129,0.588-0.029 c0.19,0.102,0.303,0.295,0.303,0.502v4.293c2.578,0.336,13.674,2.33,13.674,11.674c0,0.271-0.191,0.508-0.459,0.562 C26.18,21.891,26.141,21.891,26.105,21.891z"></path>
+               </g>
+            </g>
+         </svg>
+      </button>
+      <button class="preview-btn history-btn" on:click={() => canvas.redo()} style="transform:matrix(-1, 0, 0, 1, 0, 0);">
+         <svg fill="#ffffff" height="18px" width="18px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 26.676 26.676" xml:space="preserve" stroke="#ffffff">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+               <g>
+                  <path d="M26.105,21.891c-0.229,0-0.439-0.131-0.529-0.346l0,0c-0.066-0.156-1.716-3.857-7.885-4.59 c-1.285-0.156-2.824-0.236-4.693-0.25v4.613c0,0.213-0.115,0.406-0.304,0.508c-0.188,0.098-0.413,0.084-0.588-0.033L0.254,13.815 C0.094,13.708,0,13.528,0,13.339c0-0.191,0.094-0.365,0.254-0.477l11.857-7.979c0.175-0.121,0.398-0.129,0.588-0.029 c0.19,0.102,0.303,0.295,0.303,0.502v4.293c2.578,0.336,13.674,2.33,13.674,11.674c0,0.271-0.191,0.508-0.459,0.562 C26.18,21.891,26.141,21.891,26.105,21.891z"></path>
+               </g>
+            </g>
+         </svg>
+      </button>
+      <button class="preview-btn" on:click={preview}>XEM ẢNH</button>
     </div>
-
-    <div class="fcenter">
-      <button class="preview-btn" on:click={preview}>Tải Ảnh</button>
-    </div>
-  </div>
 {/if}
 
 {#if step === "preview"}
-  <div class="fcenter">
-    <a href={downloadUrl} download>Tải ảnh</a>
+  <h3 style="color: red;font-weight: bold;text-align: center;">
+    LƯU Ý NẾU DÙNG IPHONE <br/>
+    Nhấn giữ vào ảnh - CHỌN LƯU ẢNH
+  </h3>
+  <img class="preview-img" src={downloadUrl} alt=""/>
+  <div class="fcenter footer">
+    <a class="preview-btn" href={downloadUrl} download>TẢI ẢNH</a>
+    <button class="preview-btn danger" on:click={() => step = "edit"}>ĐÓNG</button>
   </div>
 {/if}
 {#if loading}
@@ -265,6 +208,12 @@
 </div>
 
 <style>
+  .footer { position:fixed;bottom:0px;width:100%; }
+  .preview-img {
+    max-height: 90%;
+    border: 1px solid;
+    object-fit: contain;
+  }
   .fcenter {
     display: flex;
     justify-content: center;
@@ -272,22 +221,21 @@
   input {
     display: none;
   }
-  label {
+  .form-group label {
     text-align: center;
     position: absolute;
+    cursor: pointer;
     bottom: 50px;
     margin: 10px;
     padding: 30px;
     min-width: 100px;
   }
-  a {
-    font-size: 22px;
-  }
   .preview-btn {
+    font-size: 14px;
     text-align: center;
-    bottom: 50px;
+    align-content: center;
     margin: 10px;
-    padding: 20px;
+    padding: 10px;
     min-width: 100px;
     border: none;
     background: #3498bb;
@@ -297,6 +245,7 @@
   .history-btn {
     padding: 10px;
     min-width: 60px;
+    height: 50px;
     background: #000000;
   }
   label.step1 {
@@ -309,13 +258,16 @@
     background: darkcyan;
     color: white;
   }
+  .danger {
+    background-color: red;
+  }
   .container {
     width: 100%;
     height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-content: center;
+    align-items: center;
   }
   .loader {
     border: 16px solid #f3f3f3;
